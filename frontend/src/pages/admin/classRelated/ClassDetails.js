02d8fb743ece49abd1707b03ce17e54984c5f4ae
School Admin <admin@school.com>
2026-04-17 11:45:35 +0530

    Fix all ESLint unused import errors across admin pages

--- frontend/src/pages/admin/classRelated/ClassDetails.js
+++ frontend/src/pages/admin/classRelated/ClassDetails.js
@@ -9,7 +9,7 @@ import {
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import { resetSubjects } from "../../../redux/sclassRelated/sclassSlice";
+
 import { BlueButton, GreenButton, PurpleButton } from "../../../components/buttonStyles";
 import TableTemplate from "../../../components/TableTemplate";
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
